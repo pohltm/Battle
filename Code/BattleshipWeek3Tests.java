260e8a96b97a30876a3327cfab1bce714db6a3eb
Tiffany Pohl <pohltm@rose-hulman.edu>
2012-04-27 15:13:32 -0400

    Added test cases for localization

--- Code/BattleshipWeek3Tests.java
+++ Code/BattleshipWeek3Tests.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import junit.framework.Assert;
 
@@ -106,4 +108,96 @@ public class BattleshipWeek3Tests {
 		Assert.assertTrue(result);
 	}
 	
+	Locale englishLocale = new Locale("en", "US");
+	Locale germanLocale = new Locale("de", "DE");
+	ResourceBundle englishBundle = GameStarter.setupBundle(englishLocale);
+	ResourceBundle germanBundle = GameStarter.setupBundle(germanLocale);
+	
+	@Test
+	public void testEnglishTitles() {
+		assertEquals("Start Screen", englishBundle.getString("startScreen"));
+		assertEquals("Setup Screen ", englishBundle.getString("setupScreen"));
+		assertEquals("Board Screen", englishBundle.getString("boardScreen"));
+		assertEquals("BATTLESHIP", englishBundle.getString("battleship"));
+	}
+	
+	@Test
+	public void testEnglishButtonText() {
+		assertEquals("Start", englishBundle.getString("start"));
+		assertEquals("Back", englishBundle.getString("back"));
+		assertEquals("Next", englishBundle.getString("next"));
+		assertEquals("Play", englishBundle.getString("play"));
+	}
+	
+	@Test
+	public void testEnglishLabelText() {
+		assertEquals("Enter your board size: (Width, Height) ", englishBundle.getString("enterBoardSize"));
+		assertEquals("Enter the number of ships: ", englishBundle.getString("enterNumShips"));
+		assertEquals("Length of Ship ", englishBundle.getString("shipLength"));
+	}
+	
+	@Test
+	public void testEnglishMenuText() {
+		assertEquals("Edit", englishBundle.getString("edit"));
+		assertEquals("Change Language", englishBundle.getString("changeLanguage"));
+		assertEquals("English", englishBundle.getString("english"));
+		assertEquals("Change the application's language to English", englishBundle.getString("changeToEnglish"));
+		assertEquals("German", englishBundle.getString("german"));
+		assertEquals("Change the application's language to German", englishBundle.getString("changeToGerman"));
+		assertEquals("Help", englishBundle.getString("help"));
+		assertEquals("About", englishBundle.getString("about"));
+	}
+	
+	@Test
+	public void testEnglishPopupText() {
+		assertEquals("<b>Goal:</b> To sink all of your opponent's ships by correctly guessing their location.<br>", englishBundle.getString("aboutGoal"));
+		assertEquals("<b>Setup:</b> Each player has a board with two girds, a set number of ships each with a specified size. The top grid is used to track your shots at the opponent's ships. The bottom grid is used to place your ships vertically or horizontally (not diagonally) across grid spaces, and cannot hang over the grid. Ships can touch each other, but can't both be on the same space. The bottom grid is also used to track the opponent's shots at your ships.<br>", englishBundle.getString("aboutSetup"));
+		assertEquals("<b>Play:</b> Players take turns firing a shot to attack enemy ships. On your turn, left-click on a space to shoot there. If there is no ship there, it will be a 'miss' and marked white. If there is a ship, it will be a 'hit' and marked red. The markings are the same when your opponent shoots at your ships. When every space of a ship is marked red, the ship is sunk.<br>", englishBundle.getString("aboutPlay"));
+		assertEquals("<b>Victory:</b> The first player to sink all of the other player's ships wins.", englishBundle.getString("aboutVictory"));
+		assertEquals("This Battleship application was created by Chris Hoorn, Cody Plungis, and Tiffany Pohl.", englishBundle.getString("applicationInfo"));
+	}
+	
+	@Test
+	public void testGermanTitles() {
+		assertEquals("Stellen Sie Schirm an", germanBundle.getString("startScreen"));
+		assertEquals("Einstellungs-Schirm ", germanBundle.getString("setupScreen"));
+		assertEquals("Brett-Schirm", germanBundle.getString("boardScreen"));
+		assertEquals("LINIENSCHIFF", germanBundle.getString("battleship"));
+	}
+	
+	@Test
+	public void testGermanButtonText() {
+		assertEquals("Anfang", germanBundle.getString("start"));
+		assertEquals("Rückseite", germanBundle.getString("back"));
+		assertEquals("Zunächst", germanBundle.getString("next"));
+		assertEquals("Spiel", germanBundle.getString("play"));
+	}
+	
+	@Test
+	public void testGermanLabelText() {
+		assertEquals("Tragen Sie Ihre Brettgröße ein: (Breite, Höhe) ", germanBundle.getString("enterBoardSize"));
+		assertEquals("Geben Sie die Zahl Schiffen ein: ", germanBundle.getString("enterNumShips"));
+		assertEquals("Länge von Schiff ", germanBundle.getString("shipLength"));
+	}
+	
+	@Test
+	public void testGermanMenuText() {
+		assertEquals("Redigieren Sie", germanBundle.getString("edit"));
+		assertEquals("Ändern Sie Sprache", germanBundle.getString("changeLanguage"));
+		assertEquals("Englisch", germanBundle.getString("english"));
+		assertEquals("Ändern Sie die Sprache der Anwendung zu Englisch", germanBundle.getString("changeToEnglish"));
+		assertEquals("Deutsch", germanBundle.getString("german"));
+		assertEquals("Ändern Sie die Sprache der Anwendung zum Deutschen", germanBundle.getString("changeToGerman"));
+		assertEquals("Hilfe", germanBundle.getString("help"));
+		assertEquals("Über", germanBundle.getString("about"));
+	}
+	
+	@Test
+	public void testGermanPopupText() {
+		assertEquals("<b>Ziel:</b> Zu alle Schiffe des Konkurrenten sinken, durch ihre Position richtig schätzen.<br>", germanBundle.getString("aboutGoal"));
+		assertEquals("<b>Einstellung:</b> Jeder Spieler hat ein Brett mit zwei Sticheleien, eine Satzanzahl von Schiffen jede mit einer spezifizierten Größe. Das Spitzenrasterfeld wird verwendet, um Ihre Schüsse an den Schiffen des Konkurrenten aufzuspüren. Das untere Rasterfeld wird verwendet, um Ihre Schiffe (nicht diagonal) über Rasterfeldräume vertikal oder horizontal zu setzen und kann nicht über dem Rasterfeld hängen. Schiffe können sich berühren, aber können nicht beide auf dem gleichen Raum sein. Das untere Rasterfeld wird auch verwendet, um die Schüsse des Konkurrenten an Ihren Schiffen aufzuspüren.<br>", germanBundle.getString("aboutSetup"));
+		assertEquals("<b>Spiel:</b> Die Spieler nehmen Umdrehungen einen Schuß abfeuernd zu den feindlichen Schiffen des Angriffs. Auf Ihrer Umdrehung link-klicken Sie auf einem Raum, um dort zu schießen. Wenn es kein Schiff dort gibt, ist es ein Verlust und ein signifikantes Weiß. Wenn es ein Schiff gibt, ist es ein Schlag und ein signifikantes Rot. Die Markierungen sind die selben wenn Ihre Konkurrenteneintragfäden an Ihren Schiffen. Wenn jeder Raum eines Schiffs signifikantes Rot ist, wird das Schiff gesunken.<br>", germanBundle.getString("aboutPlay"));
+		assertEquals("<b>Sieg:</b> Der erste Spieler, zum aller Schiffe des anderen Spielers zu sinken gewinnt.", germanBundle.getString("aboutVictory"));
+		assertEquals("Diese Linienschiffanwendung wurde durch Chris Hoorn, Cody Plungis und Tiffany Pohl verursacht.", germanBundle.getString("applicationInfo"));
+	}
 }
